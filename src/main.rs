3de8eb8799e299e38fa4a95620110c16951b9263
Evan Johnson <evan.durston.johnson@gmail.com>
2023-12-02 12:39:43 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![feature(test)]
+
+mod day1;
+
+const YEAR: &str = "2022";
+const COOKIE_PATH: &str = "../session.cookie";
+const INPUT_CACHE: &str = "input";
+
+fn main() {
+    println!("Hello, world!");
+}
