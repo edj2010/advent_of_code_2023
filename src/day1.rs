115f45fc9f7f8a3e498a8430583bd308df5afd97
Evan Johnson <evan.durston.johnson@gmail.com>
2023-12-02 14:28:50 -0500

    day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,22 +1,59 @@
 use advent_of_code::parse::{parsers, Parser};
 
-fn parse(input: &str) -> String {
-    parsers::any()
+#[allow(dead_code)]
+pub fn part1(input: &str) -> u32 {
+    parsers::chars(|c| c.is_alphanumeric())
+        .many()
+        .many_lines("\n")
         .parse(input)
         .finish()
         .expect("Failed to parse input")
-}
-
-#[allow(dead_code)]
-pub fn part1(input: &str) -> u32 {
-    parse(input);
-    0
+        .map(|line| {
+            let digits: Vec<u32> = line.filter_map(|digit| digit.to_digit(10)).collect();
+            digits.first().unwrap() * 10 + digits.last().unwrap()
+        })
+        .sum()
 }
 
 #[allow(dead_code)]
 pub fn part2(input: &str) -> u32 {
-    parse(input);
-    0
+    parsers::many_chars(|c| c.is_alphanumeric())
+        .many_lines("\n")
+        .parse(input)
+        .finish()
+        .expect("Failed to parse input")
+        .map(|s| {
+            let mut digits_at: Vec<Option<u32>> = vec![None; s.len()];
+            s.bytes().enumerate().for_each(|(idx, d)| {
+                (d as char)
+                    .to_digit(10)
+                    .iter()
+                    .for_each(|&d| digits_at[idx] = Some(d))
+            });
+            [
+                ("one", 1),
+                ("two", 2),
+                ("three", 3),
+                ("four", 4),
+                ("five", 5),
+                ("six", 6),
+                ("seven", 7),
+                ("eight", 8),
+                ("nine", 9),
+            ]
+            .iter()
+            .for_each(|(word, digit)| {
+                s.find(word)
+                    .iter()
+                    .for_each(|&idx| digits_at[idx] = Some(*digit));
+                s.rfind(word)
+                    .iter()
+                    .for_each(|&idx| digits_at[idx] = Some(*digit));
+            });
+            let digits: Vec<u32> = digits_at.iter().filter_map(|&d| d).collect();
+            digits.first().unwrap() * 10 + digits.last().unwrap()
+        })
+        .sum()
 }
 
 #[allow(dead_code)]
@@ -27,17 +64,37 @@ mod tests {
     use advent_of_code::{day::Day, web_api::load_question_input};
     use test::Bencher;
 
-    const EXAMPLE: &str = "";
     const DAY: Day = Day::Day01;
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(EXAMPLE), 0);
+        assert_eq!(
+            part1(
+                "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"
+            ),
+            142
+        );
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(EXAMPLE), 0);
+        assert_eq!(
+            part2(
+                "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"
+            ),
+            281
+        );
     }
 
     #[test]
@@ -49,22 +106,44 @@ mod tests {
                 crate::INPUT_CACHE,
                 DAY
             )),
-            0
+            56397
         );
     }
 
     #[test]
     fn part2_test() {
-        assert_eq!(part2(&load_input()), 0);
+        assert_eq!(
+            part2(&load_question_input(
+                crate::YEAR,
+                crate::COOKIE_PATH,
+                crate::INPUT_CACHE,
+                DAY
+            )),
+            55701
+        );
     }
 
     #[bench]
     fn part1_bench(b: &mut Bencher) {
-        b.iter(|| part1(&load_input()));
+        b.iter(|| {
+            part1(&load_question_input(
+                crate::YEAR,
+                crate::COOKIE_PATH,
+                crate::INPUT_CACHE,
+                DAY,
+            ))
+        });
     }
 
     #[bench]
     fn part2_bench(b: &mut Bencher) {
-        b.iter(|| part2(&load_input()));
+        b.iter(|| {
+            part2(&load_question_input(
+                crate::YEAR,
+                crate::COOKIE_PATH,
+                crate::INPUT_CACHE,
+                DAY,
+            ))
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 mod day1;
 
-const YEAR: &str = "2022";
+const YEAR: &str = "2023";
 const COOKIE_PATH: &str = "../session.cookie";
 const INPUT_CACHE: &str = "input";
 
